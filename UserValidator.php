732fa0a19c1e108674cf472afb34fe7e9d5d7478
KubaG1974 <kuba.gorzynski@gmail.com>
2024-09-04 18:25:18 +0200

    Implemented validateEmail method in UserValidator class with basic regex validation.

--- UserValidator.php
+++ UserValidator.php
@@ -2,10 +2,17 @@
 class userValidator {
     
     public function validateEmail(string $email): bool {
+        
+        $pattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+        return (bool)preg_match($pattern, $email);  
 
     }
 
     public function validatePassword(string $password): bool {
+        
+        if (strlen($password < 8)) {
+            return false;
+        }
         
     }
 
