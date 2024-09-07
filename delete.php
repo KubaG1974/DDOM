ba4f66ec5d45ef8c1ed33d163d11918265a4030e
KubaG1974 <kuba.gorzynski@gmail.com>
2024-09-07 19:29:59 +0200

    Added deelte.php file.

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+require 'db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM users WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['id' => $id]);
+
+    header("Location: test_2.php");
+    exit;
+}
+?>
